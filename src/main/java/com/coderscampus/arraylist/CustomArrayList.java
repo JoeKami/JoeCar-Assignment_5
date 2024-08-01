01f3ed8ea327665a7f5af253181fb8f7ea8d3607
JoeKami <j.b.carrasquillo@gmail.com>
2024-08-01 00:35:53 -0400

    Implementing methods from interface and instantiating new Custom Array List

--- src/main/java/com/coderscampus/arraylist/CustomArrayList.java
+++ src/main/java/com/coderscampus/arraylist/CustomArrayList.java
@@ -2,22 +2,32 @@ package com.coderscampus.arraylist;
 
 public class CustomArrayList<T> implements CustomList<T> {
 	Object[] items = new Object[10];
+	int size;
 
 	@Override
 	public boolean add(T item) {
-		// TODO Auto-generated method stub
-		return false;
+		if(size == items.length){
+			Object[] newItems = new Object[items.length * 2];
+			for (int i = 0; i < items.length; i++) {
+				newItems[i] = items[i];
+			}
+			items = newItems;
+		}
+		items[size] = item;
+		size++;
+		return true;
 	}
 
 	@Override
 	public int getSize() {
-		// TODO Auto-generated method stub
-		return 0;
+		return size;
 	}
 
 	@Override
 	public T get(int index) {
-		// TODO Auto-generated method stub
+		if (index >= 0 && index < size) {
+			return (T) items[index];
+		}
 		return null;
 	}
 	
